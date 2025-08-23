3b7ee4f3d250c1a621beff9dfa1c9ff9ccade1c8
Iago Vieira Carvalho <iagovelox@gmail.com>
2025-08-22 23:24:11 -0300

    a

--- TCC Final/meu-backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-router.post('/', chatController.enviarChat);
-router.get('/', chatController.listarChats);
-
-module.exports = router;
--- TCC Final/meu-backend/routes/notificacaoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificacaoController = require('../controllers/notificacaoController');
-
-router.post('/', notificacaoController.criarNotificacao);
-router.get('/:usuarioId', notificacaoController.listarNotificacoes);
-router.patch('/:id/lida', notificacaoController.marcarComoLida);
-
-module.exports = router;
